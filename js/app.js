2a3ac76831498dfce16c05c05322ac8e3ef34b68
Thiago Kuperman <kupermanthiago@gmail.com>
2022-01-19 17:33:47 -0300

    buscador

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 const carrito = document.getElementById("carrito");
 const libros = document.getElementById("lista-libros");
 const listaLibros = document.querySelector("#lista-carrito tbody");
@@ -104,7 +105,7 @@ function leerLocalStorage() {
             <td>${libro.titulo}</td>
             <td>${libro.precio}</td>
             <td>
-                <a href="#" class="borrar-platillo" data-id="${libro.id}">X</a>
+                <a href="#" class="borrar-libro" data-id="${libro.id}">X</a>
             </td>
         `;
         listaLibros.appendChild(row);
@@ -128,12 +129,100 @@ function vaciarLocalStorage() {
     localStorage.clear();
 }
 
-$(".query").css("color", "rgb(196,203,183)");
-
-$("#ocultar-foot").click(function(){
-    $("#footer").toggle("fast");
-})
 
 
+//buscador
 
+$(document).ready(function() {
+    var item;
+    var outputList = document.getElementById("resultados");
+    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    var placeHldr = '<img src="https://via.placeholder.com/150">';
+    var searchData;
+  
+    $("#buscar").click(function() {
+      outputList.innerHTML = ""; 
+      document.body.style.backgroundImage = "url('')";
+       searchData = $("#buscador").val();
+       if(searchData === "" || searchData === null) {
+         displayError();
+       }
+      else {
+         //console.log(searchData);
+        // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData());
+         $.ajax({
+            url: bookUrl + searchData,
+            dataType: "json",
+            success: function (response) {
+               // console.log(response);
+                if (response.totalItems === 0) {
+                    alert("No hay resultados!.. Prueba otra vez:)");
+                }
+                else {
+                    $("#titulo").animate({ 'margin-top': '5px' }, 1000);
+                    $(".book-list").css("visibility", "visible");
+                    displayResults(response);
+                }
+            },
+            error: function () {
+              alert("Algo salio mal..Prueba de nuevo!");
+            }
+          });
+        }
+        $("#buscador").val(""); 
+     });
+  
+     //    function para mostrar los resultados en index
+
+     function displayResults(response) {
+        for (var i = 0; i < response.items.length; i+=2) {
+          item = response.items[i];
+          title1 = item.volumeInfo.title;
+          author1 = item.volumeInfo.authors;
+          publisher1 = item.volumeInfo.publisher;
+          bookLink1 = item.volumeInfo.previewLink;
+          bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier;
+          bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
+  
+          item2 = response.items[i+1];
+          title2 = item2.volumeInfo.title;
+          author2 = item2.volumeInfo.authors;
+          publisher2 = item2.volumeInfo.publisher;
+          bookLink2 = item2.volumeInfo.previewLink;
+          bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier;
+          bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
+  
+          
+          outputList.innerHTML += '<div class="container">'+
+                                  formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
+                                  formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                                  '</div>';
+  
+          console.log(outputList);
+        }
+     }
+  
+     function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+         
+       var htmlCard = `<div class="row">
+                         <div class="for columns">
+                           <div class="card">
+                            <img src="${bookImg}" class="img-book">
+                               <div class="info-card">
+                                  <h4>${title}</h4>
+                                  <p>${author}</p>
+                                  <img src="img/estrellas.png">
+                                  <p class="precio">$50 <span class="u-pull-right">$15</span> </p>
+                                  <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="${bookIsbn}">Agregar al carrito</a>
+                                </div>
+                          </div>
+                        </div>
+                     </div>`;
+       return htmlCard;
+     }
+     function displayError() {
+       alert("La busqueda no puede estar vacia!")
+     }
+  
+  });
 
